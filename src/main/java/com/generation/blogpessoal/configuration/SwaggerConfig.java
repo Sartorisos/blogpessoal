71af20f178d5b367b3dc4f6268eb5bd32d79adc9
Fernando Sartori <fehsartorisos@gmail.com>
2023-06-25 13:30:01 -0300

    implementações;deploy

--- src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -20,18 +20,18 @@ public class SwaggerConfig {
         return new OpenAPI()
             .info(new Info()
                 .title("Projeto Blog Pessoal")
-                .description("Projeto Blog Pessoal - Generation Brasil")
+                .description("Projeto Blog Pessoal")
                 .version("v0.0.1")
                 .license(new License()
-                    .name("Generation Brasil")
-                    .url("https://brazil.generation.org/"))
+                    .name("Fernando Sartori")
+                    .url("https://github.com/Sartorisos/blogpessoal"))
                 .contact(new Contact()
-                    .name("Generation Brasil")
-                    .url("https://github.com/conteudoGeneration")
-                    .email("conteudogeneration@generation.org")))
+                    .name("Fernando Sartori")
+                    .url("https://github.com/Sartorisos/blogpessoal")
+                    .email("fehsartori34@gmail.com")))
             .externalDocs(new ExternalDocumentation()
                 .description("Github")
-                .url("https://github.com/conteudoGeneration/"));
+                .url("https://github.com/Sartorisos/blogpessoal"));
     }
 
 
